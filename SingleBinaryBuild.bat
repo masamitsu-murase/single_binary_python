a8a149028df9dfef8472fa968ffea20e3acca96f
Masamitsu MURASE <masamitsu.murase@gmail.com>
2026-02-07 03:17:02 +0900

    Call prepare_xxx.bat.

--- SingleBinaryBuild.bat
+++ SingleBinaryBuild.bat
@@ -1,3 +1,7 @@
 
 set PYTHON=C:\Users\murase\AppData\Local\Programs\Python\Python313\python.exe
-call SingleBinaryBuild\build.bat -c Release -p x64 -t Build --no-ssl --no-tkinter
+call SingleBinaryBuild\get_externals.bat --tkinter-src --openssl-src --libffi-src --no-llvm
+call SingleBinaryBuild\prepare_libffi.bat -x64 --install-cygwin
+call SingleBinaryBuild\prepare_ssl.bat
+call SingleBinaryBuild\prepare_tcltk.bat
+call SingleBinaryBuild\build.bat -c Release -p x64 -t Build
