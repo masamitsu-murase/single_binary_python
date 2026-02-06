d85ab27b3ec742cc0f078bd9e4a8018613960d7b
Masamitsu MURASE <masamitsu.murase@gmail.com>
2026-02-07 03:36:32 +0900

    Update SingleBinaryBuild.bat

--- SingleBinaryBuild.bat
+++ SingleBinaryBuild.bat
@@ -1,6 +1,8 @@
 
 set PYTHON=C:\Users\murase\AppData\Local\Programs\Python\Python313\python.exe
 call SingleBinaryBuild\get_externals.bat --tkinter-src --openssl-src --libffi-src --no-llvm
+set LIBFFI_SOURCE=%~dp0externals\libffi-3.4.4
+set "VCVARSALL=C:\Program Files (x86)\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
 call SingleBinaryBuild\prepare_libffi.bat -x64 --install-cygwin
 call SingleBinaryBuild\prepare_ssl.bat
 call SingleBinaryBuild\prepare_tcltk.bat
